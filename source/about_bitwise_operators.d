f148db1d02d4c5cb68222a6afd56fc974d30917d
nerdinand <nerdinand@nerdinand.com>
2015-04-04 14:15:59 +0200

    solving...

--- source/about_bitwise_operators.d
+++ source/about_bitwise_operators.d
@@ -11,18 +11,18 @@ class AboutBitwiseOperators
   public void shifts() {
     auto i = 1;
     auto j = -8;
-    assertEquals(j << 3, FILL_IN_THIS_NUMBER);
-    assertEquals(j >>> 3, FILL_IN_THIS_NUMBER);
-    assertEquals(j >> 3, FILL_IN_THIS_NUMBER);
-    assertEquals(i << 3, FILL_IN_THIS_NUMBER);
+    assertEquals(j << 3, -64);
+    assertEquals(j >>> 3, 536870911);
+    assertEquals(j >> 3, -1);
+    assertEquals(i << 3, 8);
   }
 
   @Test
   public void operators() {
-    assertEquals(2 | 4, FILL_IN_THIS_NUMBER); // or
-    assertEquals(3 & 5, FILL_IN_THIS_NUMBER); // and
-    assertEquals(6 ^ 4, FILL_IN_THIS_NUMBER); // xor
-    assertEquals(~3, FILL_IN_THIS_NUMBER);  // not
+    assertEquals(2 | 4, 6); // or
+    assertEquals(3 & 5, 1); // and
+    assertEquals(6 ^ 4, 2); // xor
+    assertEquals(~3, -4);  // not
   }
 
 }
