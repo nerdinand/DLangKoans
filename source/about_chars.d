54eb62ede2d2f3192fb04f6ccbf2a1b629156429
nerdinand <nerdinand@nerdinand.com>
2015-04-04 14:23:02 +0200

    solving...

--- source/about_chars.d
+++ source/about_chars.d
@@ -10,26 +10,26 @@ class AboutChars
   @Test
   public void lonely_char() {
     char a_character = 'a';
-    assertEquals(a_character, FILL_IN_THIS_CHAR);
+    assertEquals(a_character, 'a');
   }
 
   @Test
   public void chars_have_a_size() {
     char a_character = 'b';
-    assertEquals(a_character.sizeof, FILL_IN_THIS_CHAR);
+    assertEquals(a_character.sizeof, 1);
   }
 
   @Test
   public void chars_are_numbers() {
     char a_character = 'A';
-    assertEquals(cast(int)(a_character), FILL_IN_THIS_NUMBER);
+    assertEquals(cast(int)(a_character), 65);
   }
 
 
   @Test
   public void more_chars_are_a_string() {
     string some_characters = "Hello World";
-    assertEquals(some_characters.length, FILL_IN_THIS_NUMBER);
+    assertEquals(some_characters.length, 11);
   }
 
 }
