d47b4f4ff9e031142aae336ba66ff46753d0b11a
nerdinand <nerdinand@nerdinand.com>
2015-04-04 13:33:56 +0200

    solving...

--- source/about_arrays.d
+++ source/about_arrays.d
@@ -10,20 +10,20 @@ class AboutArrays
   @Test
    void fixed_length_arrays() {
     string[4] fruits = ["banana", "mango", "apple", "orange"];
-    assertEquals(fruits[0], FILL_IN_THIS_STRING);
-    assertEquals(fruits.length, FILL_IN_THIS_NUMBER);
-    int[5] b = 1; // 5 elements with same value 1
-    assertEquals(b, FILL_IN_THIS_ARRAY);
+    assertEquals(fruits[0], "banana");
+    assertEquals(fruits.length, 4);
+    int[5] b = 1;   // 5 elements with same value 1
+    assertEquals(b, [1, 1, 1, 1, 1]);
   }
 
 
   @Test
   void dynamic_length_arrays() {
     string[] fruits = ["banana", "mango"];
-    assertEquals(fruits.length, FILL_IN_THIS_NUMBER);
+    assertEquals(fruits.length, 2);
     fruits ~= "strawberry";
-    assertEquals(fruits.length, FILL_IN_THIS_NUMBER);
-    assertEquals(fruits[2], FILL_IN_THIS_STRING);
+    assertEquals(fruits.length, 3);
+    assertEquals(fruits[2], "strawberry");
   }
 
 
@@ -31,18 +31,18 @@ class AboutArrays
   void array_slicing() {
     int[] a = [4, 3, 2, 1];
     a[0..2] = [1, 2];
-    assertEquals(a, FILL_IN_THIS_ARRAY);
+    assertEquals(a, [1, 2, 2, 1]);
     a[0..$] = [0, 0, 0, 0]; // $ is always the last element
-    assertEquals(a, FILL_IN_THIS_ARRAY);
+    assertEquals(a, [0, 0, 0, 0]);
   }
 
   @Test
   void array_append() {
     int [] a;
     a.length = 3; // length extended, elements using default value
-    assertEquals(a, FILL_IN_THIS_ARRAY);
+    assertEquals(a, [0, 0, 0]);
     a ~= [3, 4];
-    assertEquals(a, FILL_IN_THIS_ARRAY);
+    assertEquals(a, [0, 0, 0, 3, 4]);
   }
 
 }
